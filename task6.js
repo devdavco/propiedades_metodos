f6bc508ff8871710b3030f45934fd8d622937355
devdavco <github.ferocity308@passmail.com>
2026-09-24 22:02:56 -0500

    task6.js done, array push

--- task6.js
+++ task6.js
@@ -1,12 +1,39 @@
 // ¡Escribe tu código debajo de esta línea!
 
-let listaCompras = [
-  [nombre = "leche", cantidad = 12],
-  [nombre = "huevos", cantidad = 12]
-];
 
-console.log(listaCompras.values)
+function DescribirLista(lista) {
+  this.lista = lista;
+  this.imprimirLista = function(){
+    
+    lista.forEach(element => {
+      console.log(element)
+    });
 
-function DescribirLista(lista) {}
+    
+  }
+}
 
+let cantidad = parseInt(process.argv[2]) //leo tamaño de la lista
+let arreglo = [];
+
+let posicion = 3
+
+for (let i = 0 ; i< cantidad ;i++ ){
+
+    const cantidadItem = Number(process.argv[posicion]);
+    const nombre = process.argv[posicion + 1];
+
+  arreglo.push({
+    cantidad: cantidadItem,
+    nombre: nombre
+  })
+    posicion +=2
+
+}
+
+
+
+const listaCompras = new DescribirLista(arreglo);
+
+listaCompras.imprimirLista()
 // ¡Escribe tu código encima de esta línea!
